23439b804d78fe446ed79303a3e0551f124bb02b
Didi Dodson <mdodson55@gmail.com>
2021-12-01 15:10:22 -0700

    Add moddel and feature tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
   end
+
+  describe 'methods' do
+    it 'excludes self from all users list' do
+      user1 = User.create!(name: "Jane Doe", email: "email1@fakeemail.com")
+      user2 = User.create!(name: "Raychaun Williams", email: "email2@fakeemail.com")
+      user3 = User.create!(name: "Tanya Rodriguez", email: "email3@fakeemail.com")
+      expect(User.all_except(user1.id)).to include(user2, user3)
+    end
+  end
 end
