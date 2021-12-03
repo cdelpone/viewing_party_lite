5aada609edc62fb2c2a318b24bc8e0a20ef56db3
Didi Dodson <mdodson55@gmail.com>
2021-12-03 07:20:41 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_012223) do
+ActiveRecord::Schema.define(version: 2021_12_03_133047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_11_30_012223) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "hosting"
   end
 
   add_foreign_key "parties", "users"
